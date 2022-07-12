15a9b9eb30fa04beeb6c7ddcfb5675988263f541
Elsammit <hhashimo25@gmail.com>
2022-07-12 21:48:51 +0900

    1 file modified

--- components/Result.vue
+++ components/Result.vue
@@ -12,14 +12,21 @@ export default {
   setup(props, context) {
     let goodMessage = ref('aaa');
     const SuccessCount = computed({
-      get: () => props.modelValue,
-      set: (value) => {
-        context.emit('update:modelValue', value);
+      get: (){
         goodMessage.value = 'good!!!';
         console.log(goodMessage.value);
         setTimeout(() => {
           goodMessage.value = '';
         }, 500);
+        return props.modelValue,
+      },
+      set: (value) => {
+        context.emit('update:modelValue', value);
+        // goodMessage.value = 'good!!!';
+        // console.log(goodMessage.value);
+        // setTimeout(() => {
+        //   goodMessage.value = '';
+        // }, 500);
       },
     });
     return { SuccessCount, goodMessage };
