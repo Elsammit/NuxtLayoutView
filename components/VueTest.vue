757afd83e36264d15aa4fe28d0dbfe8fde9b0c69
Elsammit <hhashimo25@gmail.com>
2022-08-08 23:41:44 +0900

    markdown

---
+++ components/VueTest.vue
@@ -0,0 +1,20 @@
+<template>
+    <p>{{ message }}</p>
+</template>
+
+<script>
+import { stringLiteral } from "@babel/types";
+import {ref} from "vue";
+import {marked} from "marked"
+export default{
+    props:{
+        title:String,
+    },
+    setup(props){
+        const message = marked(props.title);
+        return {
+            message
+        };
+    }
+};
+</script>
