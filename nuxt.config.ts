f39f271e78f8a489698e06f6da3c7b2fc040d1e4
Elsammit <hhashimo25@gmail.com>
2022-11-29 23:38:08 +0900

    テスト8

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,12 +8,6 @@ export const useMyFetch = (request, opts) => {
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    Plugins:[
-        {
-            src: '@/plugins/plugin',
-            mode: 'client'
-          }
-    ],
     serverMiddleware:[{
         path:"/api",
         handler:"~/express1/api.js"
