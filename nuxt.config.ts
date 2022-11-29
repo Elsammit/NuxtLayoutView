f5d59ebfdaa4cf8af4fa2afcc7c3f38ab58db062
Elsammit <hhashimo25@gmail.com>
2022-11-29 23:34:21 +0900

    プッシュ8

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 import { defineNuxtConfig } from 'nuxt'
 
+export const useMyFetch = (request, opts) => {
+    const config = useRuntimeConfig()
+  
+    return useFetch(request, { baseURL: config.public.baseURL, ...opts })
+  }
+
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     Plugins:[
@@ -12,8 +18,10 @@ export default defineNuxtConfig({
         path:"/api",
         handler:"~/express1/api.js"
     }],
-    app:{
-        baseURL:'/NuxtLayoutView/'
+    runtimeConfig:{
+        public:{
+            baseURL: process.env.BASE_URL || 'https://elsammit.github.io/NuxtLayoutView/'
+        }
     }
 })
 
